f4ba4ed93204ec9647a675151332c79e6c48830b
Dalton Burke <dmtburke@gmail.com>
2019-10-16 13:15:39 -0600

    basic shells for location classes

---
+++ src/location.py
@@ -0,0 +1,28 @@
+# Location class, subclasses, and functions
+# Dalton Burke
+
+class Location:
+    def __init__(self, address):
+        self.address = address
+
+class Landfill(Location):
+    def __init__(self, address, num6, num9, num12, num16):
+        super(self, address)
+        self.num6 = num6
+        self.num9 = num9
+        self.num12 = num12
+        self.num16 = num16
+
+class ServiceSite(Location):
+    def __init__(self, address, map_code, truck_type, delivery_time, can_size, service_type):
+        super(self, address)
+        self.map_code = map_code
+        self.truck_type = truck_type
+        self.delivery_time = delivery_time
+        self.can_size = can_size
+        self.name = name
+        self.service_type = service_type
+
+def distance(from_location, to_location):
+
+    return

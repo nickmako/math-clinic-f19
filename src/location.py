a346fb6b55ba8b43ceea21b065539b46b0c73c66
Dalton Burke <dmtburke@gmail.com>
2019-10-31 10:53:16 -0600

    added matching calculation shell (untested)

--- src/location.py
+++ src/location.py
@@ -14,11 +14,11 @@ class Landfill(Location):
         self.num16 = num16
 
 class ServiceSite(Location):
-    def __init__(self, address, map_code, truck_type, delivery_time, can_size, service_type):
+    def __init__(self, address, map_code, truck_type, service_time, can_size, service_type):
         super(self, address)
         self.map_code = map_code
         self.truck_type = truck_type
-        self.delivery_time = delivery_time
+        self.service_time = service_time
         self.can_size = can_size
         self.name = name
         self.service_type = service_type
@@ -29,3 +29,20 @@ class ServiceSite(Location):
 def distance(from_location, to_location):
 
     return
+
+def compatible(delivery, pickup, same_landfill=False):
+    if delivery.nearest_landfill != pickup.nearest_landfill and same_landfill == True:
+        return False
+
+    if delivery.service_type != "D" or pickup.service_type != "P":
+        return False
+
+    if delivery.service_time != "ANY" and \
+            pickup.service_time != "ANY" and \
+            delivery.service_time != pickup.service_time:
+        return False
+
+    # need to determine specific rules for truck types
+    #if delivery.truck_type 
+    
+    return True

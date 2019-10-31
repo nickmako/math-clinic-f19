e0570bc4c44c8fb7ef67bcf1e0a265e078708068
Dalton Burke <dmtburke@gmail.com>
2019-10-31 11:08:38 -0600

    made use of built in package tools to make code more readable

--- src/matching.py
+++ src/matching.py
@@ -2,17 +2,9 @@
 # Dalton Burke
 
 # Munkres is an implementation of the hungarian algorithm
-from munkres import Munkres
+from munkres import Munkres, DISALLOWED, make_cost_matrix
 
 # benefit is how much you gain by using the triangle over doing them individually
-# Munkres minimizes "cost", our goal is to maximize benefit, so we can
-# take a large number and subtract benefit from it, and minimize that
-
-# We also don't want to use an edge at all if it isn't possible, or if
-# the benefit is negative (ie, it's better to just do them separately)
-# so we give these edges huge costs so that it's obvious when one is chosen, and
-# so that we can remove them from the solution at the end. We have to do this
-# because Munkres returns a maximum matching (ie, everything is paired up)
 
 # list of jobs, pulled from somewhere else, empty for now
 jobs = []
@@ -20,20 +12,26 @@ jobs = []
 deliveries  = filter(lambda x: x.service_type == "D", jobs)
 pickups     = filter(lambda x: x.service_type == "P", jobs)
 
-# construct the cost matrix to feed to munkres
-cost_matrix = []
+# construct the benefit matrix to feed to munkres
+benefit_matrix = []
 
 for d in deliveries:
     row = []
     for p in pickups:
-        benefit = 1000 - (2*distance(d,d.nearest_landfill) + 2*distance(p,p.nearest_landfill)\
+        benefit = (2*distance(d,d.nearest_landfill) + 2*distance(p,p.nearest_landfill)\
                 - distance(d,d.nearest_landfill) + distance(d,p) + distance(p,p.nearest_landfill))
-        if not compatible(d,p) or benefit > 1000:
-            row.apppend(1000000)
+        if not compatible(d,p, same_landfill=True) or benefit < 0:
+            row.apppend(DISALLOWED)
         else:
             row.append(benefit)
-    cost_matrix.append(row)
+    benefit_matrix.append(row)
 
 m = Munkres()
+cost_matrix = make_cost_matrix(benefit_matrix)
 indices = m.compute(cost_matrix)
-# will need to process the output more
+
+pairs = []
+for row, col in indices:
+    pairs.append((deliveries[row],pickups[col]))
+
+# will need to process the output more after we have some data to test
